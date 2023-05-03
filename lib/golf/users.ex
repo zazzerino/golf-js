d654f2f904b2ce9a8c0dab6f2c7f1472216344e7
Kyle Patterson <patterkyle@gmail.com>
2023-05-02 19:38:38 -0700

    user schema

---
+++ lib/golf/users.ex
@@ -0,0 +1,19 @@
+defmodule Golf.Users do
+  import Ecto.Query, warn: false
+
+  alias Golf.Repo
+  alias Golf.Users.User
+
+  def get_user(user_id) do
+    Repo.get(User, user_id)
+  end
+
+  def create_user() do
+    Repo.insert(%User{})
+  end
+
+  def update_username(user_id, username) do
+    from(u in User, where: [id: ^user_id])
+    |> Repo.update_all(set: [username: username])
+  end
+end
