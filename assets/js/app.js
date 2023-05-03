9e7a15e4b841c84870a7a115f7d1c08fffa8a8ca
Kyle Patterson <patterkyle@gmail.com>
2023-05-02 17:58:12 -0700

    card moves

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,28 +3,69 @@
 // import "./user_socket.js"
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-import "phoenix_html"
+import "phoenix_html";
+import { PIXI } from "../vendor/pixi";
+import "../css/app.css";
 
-const gameContainer = document.querySelector(".game-container");
 const gameWidth = 600;
 const gameHeight = 600;
 
-const app = new PIXI.Application({width: gameWidth, height: gameHeight, backgroundColor: 0x22ffdd});
+const app = new PIXI.Application({
+  width: gameWidth, 
+  height: gameHeight, 
+  backgroundColor: 0x22ffdd,
+  antialias: true,
+});
+
+const gameContainer = document.querySelector(".game-container");
 gameContainer.appendChild(app.view);
 
-const sprite = PIXI.Sprite.from("/images/cards/AS.svg");
-sprite.scale.set(0.35, 0.35);
-sprite.anchor.set(0.5);
-sprite.x = 300;
-sprite.y = 300;
-app.stage.addChild(sprite);
+function makeCardSprite(cardName, x = 0, y = 0) {
+  const sprite = PIXI.Sprite.from(`/images/cards/${cardName}.svg`);
+  sprite.scale.set(0.4, 0.4);
+  sprite.anchor.set(0.5);
+  sprite.x = x;
+  sprite.y = y;
+  return sprite;
+}
+
+function drawDeck(container) {
+  const sprite = makeCardSprite("2B", 300, 300);
+  container.addChild(sprite);
+  return sprite;
+}
+
+const deck = drawDeck(app.stage);
 
 let elapsed = 0.0;
 
 app.ticker.add(delta => {
   elapsed += delta;
-  sprite.x = 300 + Math.cos(elapsed /50) * 100;
+  deck.x = 300 + Math.cos(elapsed / 50) * 100;
+  // deck.y = 300 + Math.cos(elapsed / 50) * 100;
 });
+
+// const cardNames = [
+//   "2B",
+//   "AC","2C","3C","4C","5C","6C","7C","8C","9C","TC","QC","KC",
+//   "AD","2D","3D","4D","5D","6D","7D","8D","9D","TD","QD","KD",
+//   "AH","2H","3H","4H","5H","6H","7H","8H","9H","TH","QH","KH",
+//   "AS","2S","3S","4S","5S","6S","7S","8S","9S","TS","QS","KS",
+// ];
+
+// for (const name of cardNames) {
+//   PIXI.Assets.add(name, `/images/cards/${name}.svg`);
+// }
+
+// const cardTextures = PIXI.Assets.load(cardNames)
+//   .then(textures => {
+//     const card = PIXI.Sprite.from(textures["AS"]);
+//     card.scale.set(0.4, 0.4);
+//     card.anchor.set(0.5);
+//     card.x = 300;
+//     card.y = 300;
+//     app.stage.addChild(card);
+//   });
 
 // Establish Phoenix Socket and LiveView configuration.
 // import {Socket} from "phoenix"
